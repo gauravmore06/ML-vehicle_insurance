46dd24713f4eaf6bfccbb26c243df9fe36de4cba
Gaurav <gauravmore0206@gmail.com>
2025-06-20 22:32:30 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Gaurav",
+    author_email="gauravmore0206@gmail.com",
+    packages=find_packages()
+)
